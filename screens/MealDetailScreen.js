becd0a71bce52a3c2fc7f64261136d39755a76fe
testsabirweb <testsabirweb@gmail.com>
2020-01-12 17:15:09 +0530

    createStackNavigator implemented

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'//it is different form 'HeaderButton' which is used in HeaderButton.js
+
+import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton'
+
+const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton} >
+      <Item title="Favorite" iconName="ios-star-outline" onPress={() => {
+        console.log('mark as favorite')
+      }} />
+    </HeaderButtons>
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default MealDetailScreen;
